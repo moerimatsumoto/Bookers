02e3f64256762a5de701c5bb8af4b733b460793c
moerimatsumoto <matsumoto.m426@gmail.com>
2021-03-30 11:36:39 +0000

    [Add]newとcreate作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,28 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
   end
 
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/:id'
   end
 
   def edit
   end
-  
-  
-  
+
+
+  private
+  def book_params
+   params.require(:book).permit(:title, :body)
+  end
 end
